ab919ba75bf6ceb8558cc8933ecabf7d3563e52c
harshitafreshcode <harshita.dhanak@freshcodes.in>
2023-10-13 19:08:18 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ const { google } = require('googleapis');
 const { PubSub } = require('@google-cloud/pubsub');
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3004;
 const SCOPES = ['https://www.googleapis.com/auth/gmail'];
 const PROJECT_ID = 'login-with-goggle-398805';
 const TOPIC_NAME = 'projects/login-with-goggle-398805/topics/email-process-demo1';
-const SUBSCRIPTION_NAME = 'projects/login-with-goggle-398805/subscriptions/email-process-sub-demo1';
+const SUBSCRIPTION_NAME = 'projects/login-with-goggle-398805/subscriptions/MySub';
 const fs = require('fs');
 const credentials = JSON.parse(fs.readFileSync('src/config/client.json'));
 const { OAuth2Client } = require('google-auth-library');
@@ -31,52 +31,73 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', routes) //main route
 
-async function watchGmail() {
-    // Set up the Gmail API client
-    // const auth = await authorize();
-    console.log('1');
-    const gmail = google.gmail({ version: 'v1', oAuth2Client });
-    console.log('2');
+// async function watchGmail() {
+//     // Set up the Gmail API client
+//     // const auth = await authorize();
+//     console.log('1');
+//     const gmail = google.gmail({ version: 'v1', oAuth2Client });
+//     console.log('2');
 
-    // Create a Pub/Sub client
-    const pubsub: any = new PubSub({ keyFilename: 'src/config/client2.json' });
+//     // Create a Pub/Sub client
+//     const pubsub: any = new PubSub({ keyFilename: 'src/config/client2.json' });
 
-    console.log('25', pubsub);
-    // Create a Pub/Sub topic
-    // const [topic] = await pubsub.createTopic(`${TOPIC_NAME}`);
-    // console.log('3');
+//     console.log('25', pubsub);
+//     // Create a Pub/Sub topic
+//     // const [topic] = await pubsub.createTopic(`${TOPIC_NAME}`);
+//     // console.log('3');
 
-    // // Create a Pub/Sub subscription
-    // await topic.createSubscription(SUBSCRIPTION_NAME);
-    // console.log('4');
+//     // // Create a Pub/Sub subscription
+//     // await topic.createSubscription(SUBSCRIPTION_NAME);
+//     // console.log('4');
 
-    // Watch the Gmail inbox
-    // const res = await gmail.users.watch({
-    //     userId: 'me',
-    //     resource: {
-    //         'labelIds': ['INBOX'],
-    //         topicName: `projects/${PROJECT_ID}/topics/${TOPIC_NAME}`,
-    //     },
-    // });
-    const res = await gmail.users.watch({
-        userId: 'me',
-        requestBody: {
-            'labelIds': ['INBOX'],
-            topicName: "projects/login-with-goggle-398805/topics/email-process"
-        },
-    });
-    console.log('Gmail watch response:', res.data);
-}
+//     // Watch the Gmail inbox
+//     // const res = await gmail.users.watch({
+//     //     userId: 'me',
+//     //     resource: {
+//     //         'labelIds': ['INBOX'],
+//     //         topicName: `projects/${PROJECT_ID}/topics/${TOPIC_NAME}`,
+//     //     },
+//     // });
+//     const res = await gmail.users.watch({
+//         userId: 'me',
+//         requestBody: {
+//             'labelIds': ['INBOX'],
+//             topicName: "projects/login-with-goggle-398805/topics/email-process"
+//         },
+//     });
+//     console.log('Gmail watch response:', res.data);
+// }
 
 
-AppDataSource.initialize().then(() => {
-    console.log("Connected to Postgres Database")
+// AppDataSource.initialize().then(() => {
+//     console.log("Connected to Postgres Database")
 
-    app.listen(port, async () => {
-        await watchGmailInbox()
-        console.log(`Server listening on port http://localhost:${port}`)
-    })
+//     // app.listen(port, async () => {
+//     //     await watchGmailInbox()
+//     //     console.log(`Server listening on port http://localhost:${port}`)
+//     // })
 
-}).catch((error) => {
-    console.log('Database Connection Failed : ', error)
+// }).catch((error) => {
+//     console.log('Database Connection Failed : ', error)
+// })
+
+const pubsub: any = new PubSub({ keyFilename: 'src/config/client2.json' });
+const subscriptionName = `${SUBSCRIPTION_NAME}`; // Replace with your subscription name
+
+const subscription = pubsub.subscription(subscriptionName);
+
+subscription.on('message', (message: any) => {
+    console.log(message, 'message');
+    const data = JSON.parse(message.data.toString());
+
+    // Handle the incoming message (notification) here, e.g., call your API.
+    console.log('Received notification:', data);
+
+    message.ack(); // Acknowledge the message to remove it from the queue
+});
+
+
+app.listen(port, async () => {
+    await watchGmailInbox()
+    console.log(`Server listening on port http://localhost:${port}`)
 })
